6c08c9c2a96ef99c6e3574f337ade2e60adb5500
rwoolive <rwoolive@utk.edu>
2016-04-21 14:04:27 -0400

    first draft of makeIndividualTree function

--- makeIndividualTree batch.R
+++ makeIndividualTree batch.R
@@ -9,7 +9,17 @@ species <- myTree$tip.label
 
 ala<-read.csv("ALA_Tassie Euc full data.csv") # environmental factors for over 70,000 eucalypt individuals across Tasmania, obtained from Atlas of Living Australia (see descriptions in "ALA and Tenure descriptions.R")
 
-alaDat <- na.omit(data.frame(alaInTree[,c("Species...matched","Temperature...annual.mean..Bio01.")]))
+alaDat <- na.omit(data.frame(ala[,c("Species...matched","Temperature...annual.mean..Bio01.")]))
 
 
-makeIndividualTree(myTree, alaDat)
+myIndTree <- makeIndividualTree(myTree, alaDat)
+
+
+
+
+
+
+
+
+
+
--- makeIndividualTree.R
+++ makeIndividualTree.R
@@ -1,15 +1,12 @@
-library(ape)
+##### final project: More than a few studies looking at variation in traits (functional traits, habitats) across species across species have failed to incorporate within-species variation in their analyses. Rather, these studies use only means of individual data to represent species values (Smith & Beaulieu 2009). However, not incorporating within species variation in these situations can lead to increased type I error rates of phylogenetic independent contrasts (up to 17%; Harmon & Losos 2005) and have considerable effect on parameter estimation (reviewed by Garamszegi 20--). Moreover, within-species variation can sometimes be greater than among-species variation, and is often biologically meaningful. Thus, it is important to incorporate within-species variation into phylogenetic comparative analyses. Although there are methods that can incorporate either standard errors or variances around species' means (Ives et al. 2007; Hansen and Bartoszek 2012), there is no method available (in R) to allow the incorporation of each individual-level datum into species-level phylogenetic analyses. I have developed a method to create individual-level phylogenies to do so, by adding tips to an inputted species-level phylogeny to represent each indivudual. This approach can be used in any phylogenetic comparative method.
 
 
-
-
-##### project 2: Studies looking at variation in traits (functional traits, habitats) across species across species fail to incorporate within-species variation in their analyses. Such variation can sometimes be greater than among-species variation, and is often biologically meaningful, thus it is important to incorporate within-species variation into phylogenetic comparative analyses. Many studies use only means of individual data to represent species values (Smith & Beaulieu 2009). However, not incorporating within species variation in these situations can lead to increased type I error rates of phylogenetic independent contrasts (up to 17%; Harmon & Losos 2005) and have considerable effect on parameter estimation (reviewed by Garamszegi 20--). Although there are methods that can incorporate either species means and standard errors or variances around those means (Ives et al. 2007; Hansen and Bartoszek 2012), there is no method available to allow the incorporation of each individual-level datum into species-level phylogenetic analyses. I have developed a method to do so, by adding tips the species-level phylogeny to represent each indivudual. Although this approach can be used in any phylogenetic comparative method, I use it here to show...
-
-
-# this function requires a phylogeny and a dataset, with the first column containing species' names and the second column containing trait values
+# this function requires a phylogeny of class 'phylo' and a dataset, in which the first column contains species' names that match the phylogeny and the second column contains trait values
 
 makeIndividualTree <- function(phy,data){
 	# first, prune the dataset to species that are in the tree
+	install.packages("ape"); library(ape)
+	install.packages("geiger"); library(geiger)
 	data <- data[data[,1] %in% phy$tip.label,] 
 	speciesInData <- unique(data[,1])
 	# then, prune the tree to species that are in the dataset
@@ -48,6 +45,7 @@ makeIndividualTree <- function(phy,data){
 		phy$tip.label[c(1:originalN)] <- paste(phy$tip.label[c(1:originalN)], "_rep1", sep="")
 		
 		# reorder dataset according to the order of species on the phylogeny:
+		numberInds <- numberInds[order(rownames(numberInds)),]
 		dataOrdered <- data[order(data[,1]),]
 		dataReps <- NA
 		for(i in 1:dim(numberInds)[1]){
@@ -55,10 +53,9 @@ makeIndividualTree <- function(phy,data){
 		}
 		dataReps <- dataReps[-1]
 		rownames(dataOrdered) <- paste(as.character(dataOrdered[,1]), "_rep", dataReps, sep="")
-		treeDat <- treedata(phy, dataOrdered)
+		treeDat <- treedata(phy, dataOrdered, sort=TRUE)
 		return(treeDat)
 }
-	
 
 
 
